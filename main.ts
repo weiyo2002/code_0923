73f1b3aac7be3b8617965ee538ad888005269811
weiyo2002 <71751264+weiyo2002@users.noreply.github.com>
2020-09-23 02:34:06 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 input.onButtonPressed(Button.A, function () {
     basic.showString("Jojo!")
 })
+input.onButtonPressed(Button.B, function () {
+    basic.showString("The World!")
+})
 basic.forever(function () {
     basic.showString("Dio!")
 })
